4d50f39401515a0bf18cc81e9eda94af558761ac
dr01d3k4 <bccooper1996@gmail.com>
2014-05-31 14:12:38 +0100

    a

--- cosmic_hack/admin.py
+++ cosmic_hack/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cosmic_hack.models import Child, NumericalQuestion, NumericalQuestionAnswer;
+from cosmic_hack.models import Child, NumericalQuestion, NumericalQuestionAnswer, QuestionOrder;
 
 
 
@@ -19,3 +19,9 @@ admin.site.register(NumericalQuestion, NumericalQuestionAdmin);
 class NumericalQuestionAnswerAdmin(admin.ModelAdmin):
 	list_display = ("id", "child");
 admin.site.register(NumericalQuestionAnswer, NumericalQuestionAnswerAdmin);
+
+
+
+class QuestionOrderAdmin(admin.ModelAdmin):
+	list_display = ("id", "type", "order", "question_number");
+admin.site.register(QuestionOrder, QuestionOrderAdmin);

bdf3a024d53da59a785a5355c89cef80c96f5871
dr01d3k4 <bccooper1996@gmail.com>
2014-05-31 16:19:37 +0100

    s

--- cosmic_hack/api.py
+++ cosmic_hack/api.py
@@ -68,38 +68,42 @@ class GetAllQuestions(View):
 
 
 # @returnHttpJson
-def postNumericalQuestionAnswer(request):
-	print("Hello");
-	# return {"result": "ok"};
-	return HttpResponse("Plain text", content_type = "text/plain");
+# def postNumericalQuestionAnswer(request):
+# 	print("Hello");
+# 	# return {"result": "ok"};
+# 	return HttpResponse("Plain text", content_type = "text/plain");
 
 
 
-# class PostNumericalQuestionAnswer(View):
-# 	@method_decorator(returnHttpJson)
-# 	def post(self, request):
-# 		# print("Hello!");
-# 		# print(request);
+class PostNumericalQuestionAnswer(View):
+	@method_decorator(returnHttpJson)
+	def post(self, request):
+		print("Hello!");
+		# print(request);
 
-# 		# question = request.POST["question"];
-# 		# answer = request.POST["answer"];
-# 		# child = None;
+		questionId = request.POST["question"];
+		answer = request.POST["answer"];
+		child = None;
 
-# 		# print(question);
-# 		# print(answer);
+		question = NumericalQuestion.objects.get(id = questionId);
+		print(question);
+		print(answer);
 
-# 		# try:
-# 		# 	childId = request.POST["child"];
-# 		# 	try:
-# 		# 		child = Child.objects.get(id = childId);
-# 		# 	except (Child.DoesNotExist):
-# 		# 		raise Http404;
-# 		# except:
-# 		# 	child = getAnonymousChild();
 
-# 		# NumericalQuestionAnswer.objects.create(question = question, answer = answer, child = child);
 
-# 		return {"result": "ok"};
+		child = getAnonymousChild();
+		# try:
+		# 	childId = request.POST["child"];
+		# 	try:
+		# 		child = Child.objects.get(id = childId);
+		# 	except (Child.DoesNotExist):
+		# 		raise Http404;
+		# except:
+		# 	child = getAnonymousChild();
+
+		NumericalQuestionAnswer.objects.create(question = question, answer = answer, child = child);
+
+		return {"result": "ok"};
 
 
 

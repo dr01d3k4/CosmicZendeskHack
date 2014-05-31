0fd28db5c2e8f42574cdeac433d803a7a33cdf02
dr01d3k4 <bccooper1996@gmail.com>
2014-05-31 12:01:20 +0100

    first api

--- cosmic_hack/urls.py
+++ cosmic_hack/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url;
-from cosmic_hack import views;
+from cosmic_hack import views, api;
 
 
 
 urlpatterns = patterns("",
 	url(r"^$", views.Index.as_view(), name = "index"),
+
+	url(r"^get-questions/$", api.getQuestions, name = "get-questions")
 );
